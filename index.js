727267a8588b8ca8c5d34c213196e5a3158ccffd
Akihiro MATSUMURA <matsumura.aki@gmail.com>
2016-07-04 00:10:41 +0900

    Custom Component for star
    For example, I use this component.
    https://github.com/oblador/react-native-vector-icons

--- index.js
+++ index.js
@@ -5,13 +5,28 @@ import React, {Component} from "react";
 class Rating extends Component {
     constructor(props) {
         super(props);
+
+        let iconWidth = this.props.iconWidth ? this.props.iconWidth : 36;
+        let iconHeight = this.props.iconHeight ? this.props.iconHeight : 36;
+
+        let defaultIconSelected = (
+            <Image
+                style={{height:iconHeight,width:iconWidth}}
+                source={require('./images/icon_star_selected.png')}/>
+        )
+        let defaultIconUnselected = (
+            <Image
+                style={{height:iconHeight,width:iconWidth}}
+                source={require('./images/icon_star_unselected.png')}/>
+        )
+
         this.state = {
             rating: this.props.rating ? this.props.rating : 0,
             max: this.props.max ? this.props.max : 5,
-            iconWidth: this.props.iconWidth ? this.props.iconWidth : 36,
-            iconHeight: this.props.iconHeight ? this.props.iconHeight : 36,
-            iconSelected: this.props.iconSelected ? this.props.iconSelected : require('./images/icon_star_selected.png'),
-            iconUnselected: this.props.iconUnselected ? this.props.iconUnselected : require('./images/icon_star_unselected.png'),
+            iconWidth: iconWidth,
+            iconHeight: iconHeight,
+            iconSelected: this.props.iconSelected ? this.props.iconSelected : defaultIconSelected,
+            iconUnselected: this.props.iconUnselected ? this.props.iconUnselected : defaultIconUnselected,
             editable: this.props.editable != null ? this.props.editable : true
         }
     }
@@ -29,11 +44,9 @@ class Rating extends Component {
             icons.push(<TouchableWithoutFeedback
                 disabled={!this.state.editable}
                 key={i}
-                style={{height:this.state.iconHeight,width:this.state.iconWidth}}
                 onPress={()=>this._onRate(i)}
             >
-                <Image style={{height:this.state.iconHeight,width:this.state.iconWidth}}
-                       source={this.state.rating>=i?this.state.iconSelected:this.state.iconUnselected}/>
+                {this.state.rating>=i ? this.state.iconSelected : this.state.iconUnselected}
             </TouchableWithoutFeedback>)
         }
         return <View style={[this.props.style,{flexDirection:'row'}]}>
